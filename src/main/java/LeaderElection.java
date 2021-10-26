e57c9f7513471809cc7dd6097021c1b6c0bbbd4f
nitinvishwari <nitinvishwari@gmail.com>
2021-10-25 23:04:04 -0400

    Leader Election part1 implemented

--- src/main/java/LeaderElection.java
+++ src/main/java/LeaderElection.java
@@ -1,20 +1,44 @@
-import org.apache.zookeeper.WatchedEvent;
-import org.apache.zookeeper.Watcher;
-import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.*;
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
 
 public class LeaderElection implements Watcher {
     private static final String ZOOKEEPER_ADDRESS = "localhost:2181";
     private static final int SESSION_TIMEOUT = 3000;
+    private static final String ELECTION_NAMESPACE = "/election";
+    private String currentZnodeName;
     private ZooKeeper zooKeeper;
-    public static void main(String [] args) throws IOException, InterruptedException {
+    public static void main(String [] args) throws IOException, InterruptedException, KeeperException {
         LeaderElection leaderElection = new LeaderElection();
         leaderElection.connectToZookeeper();
-//        leaderElection.run();
-//        leaderElection.close();
-//        System.out.println("Disconnected from Zookeeper, exiting application");
+        leaderElection.volunteerForLeadership();
+        leaderElection.electLeader();
+        leaderElection.run();
+        leaderElection.close();
+        System.out.println("Disconnected from Zookeeper, exiting application");
     }
+
+    public void volunteerForLeadership() throws InterruptedException, KeeperException {
+        String znodePrefix = ELECTION_NAMESPACE + "/c_";
+        String znodeFullPath = zooKeeper.create(znodePrefix, new byte[]{}, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
+        System.out.println("znode-name" + znodeFullPath);
+        this.currentZnodeName = znodeFullPath.replace(ELECTION_NAMESPACE + "/", "");
+    }
+
+    public void electLeader() throws InterruptedException, KeeperException {
+        List<String> children = zooKeeper.getChildren(ELECTION_NAMESPACE, false);
+        Collections.sort(children);
+        String smallestChild = children.get(0);
+
+        if(smallestChild.equals(currentZnodeName)) {
+            System.out.println("I am the leader - follow me guys");
+            return;
+        }
+        System.out.println("I("+ currentZnodeName +") am following znode -" + smallestChild);
+    }
+
     public void connectToZookeeper() throws IOException {
         this.zooKeeper = new ZooKeeper(ZOOKEEPER_ADDRESS, SESSION_TIMEOUT, this);
     }
